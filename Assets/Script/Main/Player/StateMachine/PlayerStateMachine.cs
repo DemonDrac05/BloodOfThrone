fa4775636a3001585d891fd6a2b862b29cee9069
DemonDrac05 <phantien.thang@vvk.lt>
2024-11-04 17:13:10 +0300

    Update State Machine
    Remove MonoBehaviour inheritance

--- Assets/Script/Main/Player/StateMachine/PlayerStateMachine.cs
+++ Assets/Script/Main/Player/StateMachine/PlayerStateMachine.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerStateMachine : MonoBehaviour
+public class PlayerStateMachine
 {
     public PlayerState playerState;
 
